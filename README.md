5bffff0374f517129968b12958f65f0992d99cb4
Julián García <juliangdev@gmail.com>
2020-10-20 10:29:40 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # js-livesearch
+Livesearch UI Test in JavaScript
+
+### Protect API KEY
+### - Install DOTENV
+
+```
+npm install dotenv
+```
+
+### - Use DOTENV
+.env file:
+```
+API_KEY=a8a5d630f7a58f4e5f3ea962d1252dcf
+```
+JavaScript:
+```
+require('dotenv').config()
+const API = process.env.API_KEY
+```
---
+++ app/js/functions.js
@@ -0,0 +1,44 @@
+const colorOrange = () => document.querySelector('i').style.color = '#FF7300'
+const colorGrey = () => document.querySelector('i').style.color = '#757575'
+
+const search = () => {
+    const term = document.getElementById('search').value
+
+    if (term.length <= 3){
+        document.querySelector('#result').style.display = 'none'
+        document.querySelector('#results').innerHTML = ''
+        return
+    }
+
+    const API = 'PASTE HERE YOUR API KEY'
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API}&query=${term}`
+    document.querySelector('i').className = 'fas fa-spinner fa-spin'
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => printResults(data.results, term))
+        .catch(error => console.log('ERROR:' + error.message))
+}
+
+const printResults = (results, term) => {
+    document.querySelector('i').className = 'fas fa-search'
+    document.querySelector('#result').style.display = 'none'
+    document.querySelector('#results').innerHTML = ''
+
+    const filtered = filterResults(results, term)
+    
+    filtered.forEach(({ original_title }) => {
+        document.querySelector('#result').style.display = 'block'
+        document.querySelector('#results').innerHTML += `<li>${original_title}</li>`
+    })    
+}
+
+const filterResults = (results, term) => {
+    const pattern = new RegExp(term, "i")
+    return results.filter(str => pattern.test(str.original_title))
+}
+
+const input = document.querySelector('input')
+input.addEventListener('focus', colorOrange)
+input.addEventListener('blur', colorGrey)
+input.addEventListener('keydown', search)
